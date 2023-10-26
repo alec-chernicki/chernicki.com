ddb6f4503eb956aaeef796ba4c095ed805223b58
Alec <aleccortega@gmail.com>
2023-10-26 00:09:30 -0400

    fix: overflow on blog post pages

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useMDXComponent } from "next-contentlayer/hooks";
+import clsx from "clsx";
 
 const CustomLink = (props: React.ComponentPropsWithoutRef<"a">) => {
   const href = props.href;
@@ -22,7 +23,16 @@ const CustomLink = (props: React.ComponentPropsWithoutRef<"a">) => {
 };
 
 function RoundedImage({ alt, ...props }: React.ComponentProps<typeof Image>) {
-  return <Image {...props} className="rounded-lg" alt={alt} />;
+  return (
+    <>
+      <Image
+        {...props}
+        className={clsx("rounded-lg w-full h-auto", { "mb-4": !!alt })}
+        alt={alt}
+      />
+      {alt ? <p className="text-xs text-center mt-0">{alt}</p> : undefined}
+    </>
+  );
 }
 
 const components = {
@@ -34,7 +44,7 @@ export function Mdx({ code }: { code: string }) {
   const Component = useMDXComponent(code);
 
   return (
-    <article className="prose prose-quoteless prose-neutral dark:prose-invert">
+    <article className="prose prose-quoteless prose-neutral dark:prose-invert overflow-hidden">
       <Component components={components} />
     </article>
   );
